0410e62133b75d609d5f246f8b92fabcd91499f1
taiki365 <akahoshihit0915tigers2003v@yahoo.co.jp>
2021-12-19 13:26:02 +0900

    Rubyドリル2

--- test.rb
+++ test.rb
@@ -1,3 +1,14 @@
 hash = { one:1, two:2, three:3} 
 puts hash.keys
 puts hash.values
+
+user_data = [
+  {user: {profile: {name: 'George'}}},
+  {user: {profile: {name: 'Alice'}}},
+  {user: {profile: {name: 'Taro'}}},
+ ]
+
+# 二重ハッシュの場合はキーを連続して指定する
+user_data.each do |u|
+  puts u[:user][:profile][:name]
+end
