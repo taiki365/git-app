8ca6aea2065f415a2407be9a5bf7cb050e2c12d8
taiki365 <akahoshihit0915tigers2003v@yahoo.co.jp>
2021-12-20 10:59:56 +0900

    Rubyドリル3

--- test.rb
+++ test.rb
@@ -12,3 +12,30 @@ user_data = [
 user_data.each do |u|
   puts u[:user][:profile][:name]
 end
+
+class Article
+
+  def initialize(author, title, content)
+    @author = author
+    @title = title
+    @content = content
+  end
+  
+  def author
+    @author
+  end
+
+  def title
+    @title
+  end
+
+  def content
+    @content
+  end
+
+end
+
+article = Article.new("阿部","Rubyの素晴らしさについて","Awesome Ruby!")
+puts "著者: #{article.author}"
+puts "タイトル: #{article.title}"
+puts "本文: #{article.content}"
