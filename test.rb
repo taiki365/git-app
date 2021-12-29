c3322147c62ce916636d793a951efbb92d27c96a
taiki365 <akahoshihit0915tigers2003v@yahoo.co.jp>
2021-12-29 09:42:29 +0900

    rubyドリル12

--- test.rb
+++ test.rb
@@ -122,3 +122,28 @@ end
 in1to10(5,false)
 in1to10(11,false)
 in1to10(11,true)
+
+class Fruit
+
+  def self.fresh
+    puts "採れたて新鮮な果実です"
+  end
+ 
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+ 
+  def introduce
+    puts "#{@name}は#{@price}円です"
+  end
+ end
+ 
+ apple = Fruit.new("リンゴ", 120)
+ orange = Fruit.new("オレンジ", 200)
+ strawberry = Fruit.new("イチゴ", 60)
+ 
+ Fruit.fresh
+ apple.introduce
+ orange.introduce
+ strawberry.introduce
