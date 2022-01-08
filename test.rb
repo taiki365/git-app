9ef68ef7362c034dfd70d9d80fad99b410e8ba62
taiki365 <akahoshihit0915tigers2003v@yahoo.co.jp>
2022-01-08 13:07:14 +0900

    Update test.rb

--- test.rb
+++ test.rb
@@ -184,14 +184,17 @@ missing_char('kitten', 1)
 
 #Rubyドリル14
 def police_trouble(a, b)
-  if a && b
+  if (a && b) || (!a && !b)
     puts "True"
-  elsif a | | b
-    puts "false"
   else
-   puts "True"
+   puts "false"
   end
 end
+
+# 呼び出し例
+police_trouble(true, true) 
+police_trouble(false, false)
+police_trouble(true, false)
 
 
 #Rubyドリル16
