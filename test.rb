5d2bedc60d3994b422172d0d01007cc988d19a58
taiki365 <akahoshihit0915tigers2003v@yahoo.co.jp>
2022-01-08 12:59:57 +0900

    Update test.rb

--- test.rb
+++ test.rb
@@ -117,6 +117,8 @@ else input > 10
   puts "10より大きい数字です"
 end
 
+
+#Rubyドリル10
 fruits_price = [["apple", [200, 250, 220]], ["orange", [100, 120, 80]], ["melon", [1200, 1500]]]
 
 fruits_price.each do |fruit|
