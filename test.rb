5cf3066bb0eccf58cc62baa66a2492600e4559c0
taiki365 <akahoshihit0915tigers2003v@yahoo.co.jp>
2022-01-18 09:47:57 +0900

    Update test.rb

--- test.rb
+++ test.rb
@@ -445,3 +445,9 @@ puts "検索したい数字を入力してください"
 target = gets.to_i
 number_of_elements = array.length
 
+result = binary_search(array, number_of_elements, target)
+
+if result == -1
+  puts "#{target}は配列内に存在しません"
+else
+  
