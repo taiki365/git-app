8f0a665acf6a164dd35d6dae4f390e39bda4a4c1
taiki365 <akahoshihit0915tigers2003v@yahoo.co.jp>
2022-01-20 11:50:06 +0900

    Update test.rb

--- test.rb
+++ test.rb
@@ -458,7 +458,7 @@ end
 def near_ten(num)
   total = (num/100) + (num/10 % 10) + (num % 10)
   remainder = total % 10
-  if 
+  if remainder <= 2 || remainder >= 8
     puts "True"
   else
     puts "10の倍数との差は○です"
