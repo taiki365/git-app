5f7b7c69cd243d0e843107f54488055fa172e7ba
taiki365 <akahoshihit0915tigers2003v@yahoo.co.jp>
2022-01-23 13:06:01 +0900

    Update test.rb

--- test.rb
+++ test.rb
@@ -493,6 +493,8 @@ def get_days(year, month)
   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
   if month == 2
     if year % 4 == 0
+      if
+      end
     else
       days = 28
     end
