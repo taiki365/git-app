82e8a300b088d62ad32d7b27e05f1e179877f85b
taiki365 <akahoshihit0915tigers2003v@yahoo.co.jp>
2022-01-30 13:10:20 +0900

    Rubyドリル35

--- test.rb
+++ test.rb
@@ -534,28 +534,42 @@ end
 
 
 #Rubyドリル35
-def registration_student
+def registration_student(students)
   # 生徒の名前と年齢を登録できるようにする
   student = {}
   puts '生徒名を入力してください'
+  student[:name] = gets.chomp
   puts '生徒の年齢を入力してください'
+  student[:age] = gets.chomp
 
   # 登録した生徒の国語、数学、英語の点数を登録できるようにする
   puts "国語の得点は？"
+  student[:japanese] = gets.to_i
   puts "数学の得点は？"
+  student[:math] = gets.to_i
   puts "英語の得点は？"
+  student[:english] = gets.to_i
+  students << student
+
 end
 
-def show_student_name
+def show_student_name(students)
   # 登録された生徒の名前を番号を振って表示する
+  i = 0
+  students.each do |student|
+    puts "#{i}: #{student[:name]}"
+    i += 1
+  end
   puts '見たい生徒の番号を入力してください'
+  num = gets.to_i
 
+  student = students[num]
   # 選択された生徒の名前、年齢、国語、数学、英語の点数を表示できるようにする
-  puts "名前:"
-  puts "年齢:"
-  puts "国語:"
-  puts "数学:"
-  puts "英語:"
+  puts "名前: #{student[:name]}"
+  puts "年齢: #{student[:age]}"
+  puts "国語: #{student[:japanese]}"
+  puts "数学: #{student[:math]}"
+  puts "英語: #{student[:english]}"
 end
 
 students = []
@@ -567,9 +581,9 @@ while true
   puts '[3]終了する'
   input = gets.to_i
   if input == 1
-    registration_student
+    registration_student(students)
   elsif input == 2
-    show_student_name
+    show_student_name(students)
   elsif input == 3
     exit
   else
